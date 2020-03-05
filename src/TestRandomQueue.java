42e48b10ea4a7a041cc317b1c2148ba7f5a491b6
Abel Akponine <60473441+abelakponine@users.noreply.github.com>
2020-03-05 13:41:28 +0000

    Junit Test Update

--- src/TestRandomQueue.java
+++ src/TestRandomQueue.java
@@ -1,5 +1,11 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class TestRandomQueue {
 	private static RandomQueue<String> queue = new RandomQueue<>();
+	
+	@Test
 	public static void main(String[] args) {
 		queue.enqueue("Abel");
 		queue.enqueue("Moses");
@@ -9,8 +15,17 @@ public class TestRandomQueue {
 		queue.dequeue();
 		queue.dequeue();
 		
+		try {
+			assertEquals((long) 3, (long) queue.size());
+			System.out.println("True\n");
+		}
+		catch(AssertionError e) {
+			System.out.println(e);
+		}
+		
 		for (Object x:queue.getList()) {
 			System.out.println((String) x);
 		}
+		
 	}
 }
